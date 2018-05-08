6410fcf3665ca1dd642f41a4eb7a9bd1494da2c1
Quentin Huys <qhuys@biomed.ee.ethz.ch>
2018-05-08 19:28:12 -0400

    removed exist statements to speed up simulation in twostep model

--- mTwostep/simulateTwostep.m
+++ mTwostep/simulateTwostep.m
@@ -1,11 +1,11 @@
 function [a,spr] = simulateTwostep(pa,s,trans,rewprob)
 
-if ~exist('rewprob'); 
-	a = sum(rand>cumsum([0 pa']));
+a = sum(rand>cumsum(pa'))+1;
+
+if nargin==4% try level 2 with rewprob 
+	spr = rewprob(s,a)>rand;
+else % if level 1 - when rewprob not provided 
 	if trans; if a==1; spr = 1; else spr = 2; end
 	else    ; if a==1; spr = 2; else spr = 1; end
 	end
-else
-	a = sum(rand>cumsum([0 pa']));
-	spr = rewprob(s,a)>rand;
 end
