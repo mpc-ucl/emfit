8b21ada72ff01ec9062b69e73462769478c020df
Isabel Berwian <berwian@biomed.ee.ethz.ch>
2020-02-17 20:18:12 +0100

    added generateDataDDM

---
+++ mEffortDDM/libDDM/generateDataDDM.m
@@ -0,0 +1,29 @@
+function [asurr, simTime] = generateDataDDM(combined_t, combined_prob,ndt)
+dt = 0.01;
+
+% make cdf of probability distribution over low and high choices
+cdf_prob = cumsum(combined_prob); 
+
+% get time corresponding to randomly choosen prob value of cdf
+r1 = rand(1);
+A = cdf_prob < r1;   
+    try 
+        simTime = abs(combined_t(sum(A)+1)-dt/2)+ndt;
+    catch 
+        % in case highest interval was choosen
+        simTime = abs(combined_t(sum(A))-dt/2)+ndt;
+    end
+        
+% get choice corresponding to same randomly choosen prob value of cdf         
+    try 
+        if combined_t(sum(A)) > 0 % if time is positive, choice is high 
+            asurr = 2;
+        else
+            asurr = 1;
+        end
+    catch
+        % if time is 0
+        asurr = 2; 
+    end
+end
+   
