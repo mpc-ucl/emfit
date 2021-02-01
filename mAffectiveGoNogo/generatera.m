e264cd6a9e75a0fc3bd380598d948b5c90b26a10
Quentin Huys <qhuys@biomed.ee.ethz.ch>
2021-02-01 23:26:15 +0000

    correct a data generation bug in affective gonogo surrogate data model

--- mAffectiveGoNogo/generatera.m
+++ mAffectiveGoNogo/generatera.m
@@ -6,20 +6,22 @@ ago = 1;
 
 a = sum(rand>cumsum([0 pa]));
 
+rn = rand<p; 
+
 if s==1	% go to win 
-	if a == ago; 	r = c*(rand<p);
-	else 			r = 0; 
+	if a == ago; 	r = c*rn; 
+	else 				r = c*(1-rn);
 	end
-elseif s==2; 	% go to avoid 
-	if a == ago; 	r = 0; 
-	else 			r = -c*(rand<p);
+elseif s==2		% nogo to win 
+	if a == ago; 	r = c*(1-rn);
+	else 				r = c*rn;
 	end
-elseif s==3		% nogo to win 
-	if a == ago; 	r = 0; 
-	else 			r = c*(rand<p);
+elseif s==3; 	% go to avoid 
+	if a == ago; 	r = -c*(1-rn);
+	else 				r = -c*rn;
 	end
 elseif s==4; 	% nogo to avoid 
-	if a == ago; 	r = -c*(rand<p);
-	else 			r = 0; 
+	if a == ago; 	r = -c*rn;
+	else 				r = -c*(1-rn);
 	end
 end
